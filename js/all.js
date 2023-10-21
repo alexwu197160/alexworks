70c641a03320188a44c09729b3ec3c7561413d39
alex <alexwu71@gmail.com>
2023-10-21 17:26:34 +0800

    newsmoving

--- js/all.js
+++ js/all.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $('.iconMoving').hide();
     $(window).on("scroll", function() {
-        $('.iconMoving').delay(3000).fadeIn(4000);
+        $('.iconMoving').delay(1000).fadeIn(4000);
     });
     $('.resumeList').hide();
     $(window).on("scroll", function() {
-        $('.resumeList').delay(6000).fadeIn(4000);
+        $('.resumeList').delay(3000).fadeIn(4000);
     });
     $('.gallery').hide();
     $(window).on("scroll", function() {
-        $('.gallery').delay(9000).slideDown(4000);
+        $('.gallery').delay(6000).slideDown(4000);
     });
 });
 $(document).ready(function() {
     $('.iconService').hide();
-    $('.iconService').delay(3000).fadeIn(4000);
+    $('.iconService').delay(4000).fadeIn(4000);
 });
